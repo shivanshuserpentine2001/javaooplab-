b59bc842dbfbe815f4086342b7123fd29f80a938
shivanshuserpentine2001 <63629636+shivanshuserpentine2001@users.noreply.github.com>
2020-12-11 11:00:04 +0530

    Add files via upload

---
+++ lab6main/externalsm.java
@@ -0,0 +1,11 @@
+package see;
+import ciem.*;
+public class externalsm extends CIE{
+	public static int[] seem= new int [5];
+	public externalsm(int usn, String name,int sem,int [] seem) {
+		super(usn,name,usn);
+		this.seem=seem;
+	}
+	}
+
+
